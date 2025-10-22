ed8147b5b18a4253a68eb5b8f5d1f736cf4894e0
Williweb <willieduards@gmail.com>
2025-10-21 23:20:37 -0600

    agregue librerias al main

--- src/main/java/com/mycompany/videoteca/poo/VideotecaPOO.java
+++ src/main/java/com/mycompany/videoteca/poo/VideotecaPOO.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 public class VideotecaPOO {
-//importe las librerias a usar
+//importe las librerias a 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //bhjgjh
