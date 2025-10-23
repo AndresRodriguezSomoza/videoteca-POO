4f0a80739eaa06789daa146c2aea18051a88117c
Williweb <willieduards@gmail.com>
2025-10-23 05:57:31 -0600

    MAterial escrito

--- src/main/java/com/mycompany/videoteca/poo/MaterialEscrito.java
+++ src/main/java/com/mycompany/videoteca/poo/MaterialEscrito.java
@@ -1,5 +1,24 @@
 package com.mycompany.videoteca.poo;
 
-public abstract class MaterialEscrito extends Material {
-    
+public abstract class MaterialEscrito {
+    protected String titulo;
+    protected String autor;
+    protected int anio;
+    protected String editorial;
+
+    public MaterialEscrito(String titulo, String autor, int anio, String editorial) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+        this.editorial = editorial;
+    }
+
+    // Métodos Getters
+    public String getTitulo() { return titulo; }
+    public String getAutor() { return autor; }
+    public int getAnio() { return anio; }
+    public String getEditorial() { return editorial; }
+
+    // Método abstracto (cada tipo mostrará su información)
+    public abstract String mostrarInfo();
 }
