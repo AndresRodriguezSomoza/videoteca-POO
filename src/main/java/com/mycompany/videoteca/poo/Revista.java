14f63f5a3d0779df76e6cf9a171f453b8bb81818
Williweb <willieduards@gmail.com>
2025-10-23 18:53:17 -0600

    Aqui esta Revista

--- src/main/java/com/mycompany/videoteca/poo/Revista.java
+++ src/main/java/com/mycompany/videoteca/poo/Revista.java
@@ -1,5 +1,25 @@
 package com.mycompany.videoteca.poo;
 
 public class Revista extends MaterialEscrito {
-    
+    private int numeroEdicion;
+    private String categoria;
+
+    public Revista(String titulo, String autor, int anio, String editorial, int numeroEdicion, String categoria) {
+        super(titulo, autor, anio, editorial);
+        this.numeroEdicion = numeroEdicion;
+        this.categoria = categoria;
+    }
+
+    public int getNumeroEdicion() { return numeroEdicion; }
+    public String getCategoria() { return categoria; }
+
+    @Override
+    public String mostrarInfo() {
+        return "📰 REVISTA\nTítulo: " + titulo +
+               "\nAutor: " + autor +
+               "\nAño: " + anio +
+               "\nEditorial: " + editorial +
+               "\nEdición: " + numeroEdicion +
+               "\nCategoría: " + categoria;
+    }
 }
