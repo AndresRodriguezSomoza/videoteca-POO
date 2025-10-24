071652075ec313dcfd47a171ebe4d933bb2a918a
Williweb <willieduards@gmail.com>
2025-10-23 18:51:02 -0600

    aqui esta libro

--- src/main/java/com/mycompany/videoteca/poo/Libro.java
+++ src/main/java/com/mycompany/videoteca/poo/Libro.java
@@ -1,5 +1,25 @@
 package com.mycompany.videoteca.poo;
 
 public class Libro extends MaterialEscrito {
-    
+    private String genero;
+    private int paginas;
+
+    public Libro(String titulo, String autor, int anio, String editorial, String genero, int paginas) {
+        super(titulo, autor, anio, editorial);
+        this.genero = genero;
+        this.paginas = paginas;
+    }
+
+    public String getGenero() { return genero; }
+    public int getPaginas() { return paginas; }
+
+    @Override
+    public String mostrarInfo() {
+        return "📖 LIBRO\nTítulo: " + titulo +
+               "\nAutor: " + autor +
+               "\nAño: " + anio +
+               "\nEditorial: " + editorial +
+               "\nGénero: " + genero +
+               "\nPáginas: " + paginas;
+    }
 }
